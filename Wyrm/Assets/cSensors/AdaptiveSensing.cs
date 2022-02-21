f66e991be36748f49b8010ff873a44dc141fd859
oboforty <rajmund.csombordi@gmail.com>
2022-02-21 17:17:33 +0100

    Added so test

---
+++ Wyrm/Assets/cSensors/AdaptiveSensing.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Sensing profile", menuName = "Sensing Profile/Adaptive Sensing", order = 1)]
+public class AdaptiveSensing : SensingProfile
+{
+    public string gecije = "22";
+}
---
+++ Wyrm/Assets/cSensors/PresenceSensing.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Sensing profile", menuName = "Sensing Profile/Presence Sensing", order = 1)]
+public class PresenceSensing : SensingProfile
+{
+    public int faszom = 3;
+}
